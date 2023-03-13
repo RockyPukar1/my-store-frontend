bed14ce571dead69c47a194668f89a2db645d152
Rocky Pukar <ckrp208@gmail.com>
2023-03-14 03:57:01 +0545

    58:32

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,56 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom/dist";
+
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import Search from "./Search/Search";
+import Cart from "../Cart/Cart";
+import { Context } from "../../utils/context";
+
 import "./Header.scss";
 const Header = () => {
-    return <div>Header</div>;
+    const [scrolled, setScrolled] = useState(false);
+    const handleScroll = ()  => {
+        const offset = window.scrollY
+        if (offset > 200) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    }
+    
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, [])
+    
+    return (
+        <header className={`main-header w-100 ${scrolled ? 'sticky-header' : ''}`}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="position-relative header-content d-flex justify-content-between align-items-center">
+                            <ul className="left text-uppercase list-unstyled d-flex gap-3 mb-0">
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Categories</li>
+                            </ul>
+                            <div className="center">My Store</div>
+                            <div className="right d-flex align-items-center gap-2">
+                                <TbSearch />
+                                <AiOutlineHeart />
+                                <span className="cart-icon position-relative">
+                                    <CgShoppingCart />
+                                    <span className="text-center position-absolute">5</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
